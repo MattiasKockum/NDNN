711e425b52b5c9ddeab1a84d0a4c453559d51cca
Mattias <mattias@kockum.net>
2021-08-11 18:58:06 +0200

    took out useless prints

--- LibProblem/Tournament.py
+++ LibProblem/Tournament.py
@@ -3,17 +3,13 @@
 import random
 
 def play_match(L, P1, P2):
-    print("match entre {} et {}".format(P1, P2))
     winner = random.choice([P1, P2])
-    print("winner : {}".format(winner))
     L[winner] += 1
 
 
 def play_tournament_recursive(L, index_min, index_max):
-    #print("tournament dans {}".format(L[index_min:index_max+1]))
     l = index_max - index_min + 1
     if l == 1:
-        print("{} play seul".format(index_max))
         L[index_min] += 1
     elif l == 2:
         play_match(L, index_min, index_max)
@@ -24,10 +20,7 @@ def play_tournament_recursive(L, index_min, index_max):
         Ll = L[index_min+int(l/2) : index_max+1]
         winner_left = Lr.index(max(Lr)) + index_min
         winner_right = Ll.index(max(Ll)) + index_min + int(l/2)
-        print("winner_left : {} ; winner_right : {}".format(
-            winner_left, winner_right))
         play_match(L, winner_right, winner_left)
-    print(L)
 
 def play_tournament(L):
     play_tournament_recursive(L, 0, len(L)-1)
